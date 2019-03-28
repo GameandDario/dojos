65c19f8495ac26242486b77a9002b452f4c57a08
David <eth3rnit3@outlook.fr>
2019-03-28 08:46:19 +0100

    add fibo dojo

---
+++ 3-fibonacci/fibo.js
@@ -0,0 +1,18 @@
+// Ecrire une fonction qui prendra un nombre en argument
+// La fonction devra retourner une chaine de charactere représentant 
+// la suite de Fibonnaci avec autant d'occurence que le nombre passé en paramètre
+const fibonacci = (maxValue) => {
+  let last = 0;
+  let current = 1;
+  let result = '';
+  for (let i = 0; i < maxValue; i++) {
+    let mem = current;
+    result += `${(last + current)}-`;
+    current = current + last;
+    last = mem;
+  }
+  return result;
+};
+
+
+console.log(fibonacci(10));

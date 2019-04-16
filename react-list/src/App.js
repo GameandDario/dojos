f6058754d5792de1e055996107f24b3ae2844bcd
Wilder <eth3rnit3@outlook.fr>
2019-04-16 15:34:53 +0200

    react-list

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-import users from './utils/users';
+import emails from './utils/users';
 
-const usersList = users(50);
+const emailsList = emails(50);
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      emails: []
+    }
+  }
+  handleClick() {
+    // Si les states ont un array faire un spread operator pour les mettre à jour.
+    this.setState({ emails: [...emailsList]})
+  }
+  handleDelete(idx) {
+    let emails = this.state.emails
+    emails.splice(idx,1)
+    this.setState({emails: [...emails]})
+  }
   render() {
-    console.log(usersList);
+    console.log(this.state);
     return (
       <div className="App">
         <h1>Welcome in users list</h1>
+        <button onClick={() => this.handleClick()}>Afficher</button>
+        <ul>
+          
+            {
+              this.state.emails.map((email, idx)=>{
+                return <li key={idx}>{email}<button onClick={() => this.handleDelete(idx)}>Supprimer</button></li>
+              })
+            }
+          
+
+        </ul>
       </div>
     );
   }
